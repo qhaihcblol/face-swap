971ac65217d9198e96784f198b0cef038ce0f934
pyQT_Hai <102220013@sv1.dut.udn.vn>
2026-05-14 14:20:35 +0700

    first commit

---
+++ src/models/blendswap.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from ..models.generator import ADDGenerator
+from ..models.encoder import MultilevelAttributesEncoder
+from ..models.iresnet import iresnet100
+
+
+class Normalize(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalize, self).__init__()
+        self.mean = mean
+        self.std = std
+
+    def forward(self, x):
+        x = x - self.mean
+        x = x / self.std
+        return x
+
+
+class BlendSwap(nn.Module):
+    def __init__(self):
+        super(BlendSwap, self).__init__()
+        self.G_ema = ADDGenerator(512, 3)
+        self.E_ema = MultilevelAttributesEncoder()
+
+        Z_e = iresnet100(pretrained=False, fp16=False)
+        Z_e = nn.Sequential(Normalize(0.5, 0.5), Z_e)
+        self.Z_e = Z_e
+
+        self.mask_head_ema = nn.Conv2d(64, 1, 1)
+
+    def forward(self, target_img, source_img):
+        E = self.E_ema
+        G = self.G_ema
+        mask_head = self.mask_head_ema
+        self.Z_e.eval()
+        G.eval()
+        E.eval()
+        mask_head.eval()
+
+        with torch.no_grad():
+            z_id = self.Z_e(source_img)
+            z_id = F.normalize(z_id)
+            feature_map_t = E(target_img)
+            output_g = G(z_id, feature_map_t)
+
+            mask = mask_head(feature_map_t[-1]).sigmoid()
+            output = output_g * mask + target_img * (1 - mask)
+        return output
